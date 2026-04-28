ca4010296462711fe26ff34ced4323f796fe0b9f
ab4280063-prog <ab4280063@gmail.com>
2026-04-28 19:24:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
     // =========================
     // SISTEMA DE ATUALIZAÇÃO
     // =========================
-const APP_VERSION = "1.0.3";
+const APP_VERSION = "1.0.4";
 
 async function checkUpdate() {
 
